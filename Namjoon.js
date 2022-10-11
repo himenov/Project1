1169a3f315b13a3dfdebdab49bcc1d6ae6da78c6
Putri Novianti <putrin86@gmail.com>
2022-10-11 12:52:28 -0500

    oneNJK

---
+++ Namjoon.js
@@ -0,0 +1,10 @@
+var audio = document.getElementsByTagName("audio")[1];
+audio.play();
+
+var audio = $("#heart")[1];
+audio.play();
+
+var audio = $("#heart")[1];
+$("nav a").mouseenter (function() {
+    audio.play();
+});
